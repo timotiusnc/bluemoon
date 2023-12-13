5d28b1ffb163f2f57821f9cd8d52477e6d73c6a4
timotiusnc <timotius.nc@gmail.com>
2023-12-13 22:45:18 +0700

    Adjust not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -17,7 +17,7 @@ export default function NotFound() {
         Page not found
       </h1>
       <p className="mt-3 text-sm text-gray-700">
-        Sorry, we couldn’t find the page you’re looking for.
+        Sorry, we couldn’t find the <s>page</s> diamond you’re looking for.
       </p>
       <Button href="/" className="mt-10">
         Go back home
